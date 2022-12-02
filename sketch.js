a4445b50f0a424e805d15d5d31d9ba6098c93de9
nathanriovictor <118491013+nathanriovictor@users.noreply.github.com>
2022-12-01 18:31:29 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,38 @@
-var ball 
 
 function setup() {
   createCanvas(400,400);
-  ball = createSprite(200,200,20,20)
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background("YELLOW");
-  if (keyIsDown(RIGHT_ARROW)) {
-    background("RED");
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
   drawSprites();
 }
-
-
-
 
